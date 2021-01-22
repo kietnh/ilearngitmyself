854a6f1a42029eae727610ecc89500e7271cbe1b
Kiet Ngo <kiet@kietnh.com>
2021-01-22 14:18:22 +0700

    Create sourcecodewithLF.php

---
+++ sourcecodewithLF.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        Commands\DailyMaintenance::class,
+        Commands\WeeklyyEmail::class,
+    ];
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        $schedule->command('daily-maintenance')->dailyAt('02:28');
+
+        // $schedule->command('similarity-scores-company all all')->dailyAt('8:28');
+
+        $schedule->command('send:email')->dailyAt('02:28');
+
+        // $schedule->command('similarity-scores-product all all 10')->dailyAt('11:28');
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__.'/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
